00e39bb46644bdff4a061a3619cc0554919c2418
Despria <wnsduf000050@gmail.com>
2021-11-05 00:36:50 +0900

    add travis.yml

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,13 +8,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://Despria.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Despria', // Usually your GitHub org/user name.
+  projectName: 'Despria.github.io', // Usually your repo name.
 
   presets: [
     [
